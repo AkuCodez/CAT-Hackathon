e6d13ffd36640d5b7531a50e68b1bec47a3fb575
AkuCodez <akshaj.tiwari1304@gmail.com>
2024-08-09 19:02:48 +0530

    basic fixes

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'client','views'));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'client','public')));
+
+app.get('/', (req, res) => {
+  res.render('signin'); // Render the 'index.ejs' file
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
