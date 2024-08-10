d242c9ec5dfce5d254b876c553bc7abea47c5e1f
Pavan2027 <pavandumesh2027@gmail.com>
2024-08-10 10:25:59 +0530

    add auth

---
+++ config/firebase-config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAK5hfNtm_yVCEkCPvE7FovEQEHO5Wq9rQ",
+    authDomain: "sammard-dcd0f.firebaseapp.com",
+    projectId: "sammard-dcd0f",
+    storageBucket: "sammard-dcd0f.appspot.com",
+    messagingSenderId: "674752454232",
+    appId: "1:674752454232:web:f40683a72c329cfd13b42d",
+    measurementId: "G-0GCJVFYLTR"
+};
+
+
+export default firebaseConfig;
